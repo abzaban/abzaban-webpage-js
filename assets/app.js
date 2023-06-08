e1387feb6213ba5776e62153689bdc72cae18365
abzaban <abrahamberrelleza10@outlook.com>
2023-06-07 17:03:33 -0700

    first version finished --stable

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+const elementsArray = document.querySelectorAll(".tile");
+const fadeIn = () => {
+    for (let i = 0; i < elementsArray.length; i++) {
+        let elem = elementsArray[i]
+        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 500;
+        if (distInView < 0)
+            elem.classList.add("inView");
+        else
+            elem.classList.remove("inView");
+    }
+}
+
+document.getElementsByTagName('section')[0].addEventListener('scroll', fadeIn);
+
+fadeIn();
